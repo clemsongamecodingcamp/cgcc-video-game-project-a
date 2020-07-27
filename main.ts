527ecc9f17d79d40d75ba5aed893299b589bae7f
SarahScibelli <68613488+SarahScibelli@users.noreply.github.com>
2020-07-27 15:45:54 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
+game.onGameUpdateWithHeading(function () {
+    controller.moveSprite(UFO, 50, 50)
+    console.logValue("x", UFO.x)
+    console.logValue("y", UFO.y)
+})
 function Start_Screen () {
     game.showLongText("This is my game. - By a Clemson First-Year Student", DialogLayout.Bottom)
 }
+let UFO: Sprite = null
 tiles.setTilemap(tiles.createTilemap(hex`0a00070004030303030303030309020101010101010101080201010d01010b010108020101010101010101080201010a01010c0101080201010101010101010806070707070707070705`, img`
     . . . . . . . . . . 
     . . . . . . . . . . 
@@ -10,7 +16,7 @@ tiles.setTilemap(tiles.createTilemap(hex`0a0007000403030303030303030902010101010
     . . . . . . . . . . 
     . . . . . . . . . . 
     `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile5,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile3,myTiles.tile4,myTiles.tile7,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14], TileScale.Sixteen))
-let UFO = sprites.create(img`
+UFO = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -28,6 +34,7 @@ let UFO = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+UFO.setPosition(80, 30)
 let Black_Hole = sprites.create(img`
     . . . 2 2 2 2 2 2 2 2 2 2 . . . 
     . . 2 f f f f f f f f f f 2 . . 
@@ -46,3 +53,4 @@ let Black_Hole = sprites.create(img`
     . . 2 f f f f f f f f f f 2 . . 
     . . . 2 2 2 2 2 2 2 2 2 2 . . . 
     `, SpriteKind.Player)
+Black_Hole.setPosition(80, 58)
