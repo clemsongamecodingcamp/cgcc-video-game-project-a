17dda2e7f5163536cb30317fea4154fd0f8cc466
JacksonSt7 <68613602+JacksonSt7@users.noreply.github.com>
2020-07-27 14:19:17 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function Start_Screen () {
     game.showLongText("This is my game. - By a Clemson First-Year Student", DialogLayout.Bottom)
 }
-tiles.setTilemap(tiles.createTilemap(hex`0a00070004030303030303030309020101010101010101080201010a01010b010108020101010101010101080201010a01010a0101080201010101010101010806070707070707070705`, img`
+tiles.setTilemap(tiles.createTilemap(hex`0a00070004030303030303030309020101010101010101080201010f01010b010108020101011210010101080201010a01010e0101080201010101010101010806070707070707070705`, img`
     . . . . . . . . . . 
     . . . . . . . . . . 
     . . . . . . . . . . 
@@ -9,7 +9,7 @@ tiles.setTilemap(tiles.createTilemap(hex`0a0007000403030303030303030902010101010
     . . . . . . . . . . 
     . . . . . . . . . . 
     . . . . . . . . . . 
-    `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile5,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile3,myTiles.tile4,myTiles.tile7,myTiles.tile11,myTiles.tile12], TileScale.Sixteen))
+    `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile5,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile3,myTiles.tile4,myTiles.tile7,myTiles.tile11,myTiles.tile12,myTiles.tile10,*1,*2,*3,*4,*5,*6], TileScale.Sixteen))
 let UFO = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
