ec869ad4f0d5a64dc8d8cf80f2c9cb76fb03ff0e
JacksonSt7 <68613602+JacksonSt7@users.noreply.github.com>
2020-07-29 15:44:59 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -102,7 +102,10 @@ scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
 })
 function Start_Screen () {
     Instructions("Welcome to Black Hole Apocalypse")
+    Instructions("A Black Hole is ravaging the universe and it is your job to avoid being hit by anything being sucked into it")
     Instructions("Use the Arrow Keys To Move")
+    Instructions("Avoid meteors and other objects flying towards the black hole")
+    Instructions("You have three lives. After one is lost you will be transported to the center of the black hole! Quickly escape from it before it's too late! ")
 }
 function SpawnMeteor () {
     meteor = sprites.create(img`
@@ -145,8 +148,7 @@ let meteor: Sprite = null
 let UFO: Sprite = null
 let Black_Hole: Sprite = null
 let IsTimeStarted = false
-Start_Screen()
-tiles.setTilemap(tiles.createTilemap(hex`0a00070004030303030303030309020101010101010101080201010d01010b010108020101010101010101080201010a01010c0101080201010101010101010806070707070707070705`, img`
+tiles.setTilemap(tiles.createTilemap(hex`0a00070004030303030303030309020101010101010101080201010d0e0e0b010108020101010e0e010101080201010a0e0e0c0101080201010101010101010806070707070707070705`, img`
     . . . . . . . . . . 
     . . . . . . . . . . 
     . . . . . . . . . . 
@@ -154,25 +156,25 @@ tiles.setTilemap(tiles.createTilemap(hex`0a0007000403030303030303030902010101010
     . . . . . . . . . . 
     . . . . . . . . . . 
     . . . . . . . . . . 
-    `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile5,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile3,myTiles.tile4,myTiles.tile7,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14], TileScale.Sixteen))
+    `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile5,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile3,myTiles.tile4,myTiles.tile7,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15], TileScale.Sixteen))
 IsTimeStarted = false
 Black_Hole = sprites.create(img`
-    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 c 2 c 2 2 f c c 2 . . . 
     . . 2 f f f f f f f f f f 2 . . 
-    . 2 f f f f f f f f f f f f 2 . 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    2 f f f f f f f f f f f f f f 2 
-    . 2 f f f f f f f f f f f f 2 . 
+    . 2 f f f f a f f a c f f f 2 . 
+    2 f f a f f f f f f f f f f f 2 
+    c f f f f f f c c f f f f c f c 
+    c f f f b f f f f f f f f a f 2 
+    2 f f f c f f c f f f f f f f c 
+    2 f a f f f f f f c f b f f f f 
+    f f c f f f c f f f f f f c f c 
+    c f f f b f f f c f f f f f f 2 
+    c f f f f f f f f f f c f f f c 
+    2 f a f f f c f f b f f f f f 2 
+    2 f f f f f f f f f f f a f f 2 
+    . 2 f f a f f a c f f a f f 2 . 
     . . 2 f f f f f f f f f f 2 . . 
-    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 c c c f 2 2 2 c c . . . 
     `, SpriteKind.neutral)
 Black_Hole.setPosition(80, 58)
 UFO = sprites.create(img`
@@ -196,10 +198,17 @@ UFO = sprites.create(img`
 UFO.setPosition(80, 30)
 info.setLife(3)
 info.setScore(0)
-game.onUpdateInterval(5000, function () {
+Start_Screen()
+game.onUpdateInterval(randint(1000, 5000), function () {
     SpawnMeteor()
+})
+game.onUpdateInterval(randint(1000, 5000), function () {
     SpawnMeteor2()
+})
+game.onUpdateInterval(randint(1000, 5000), function () {
     SpawnMeteor3()
+})
+game.onUpdateInterval(randint(1000, 5000), function () {
     SpawnMeteor4()
 })
 game.onUpdateInterval(500, function () {
